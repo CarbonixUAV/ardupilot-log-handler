54b4e1657fe09963a34d3a6359b9a38a73d1a71d
loki077 <loki077@gmail.com>
2025-04-06 10:15:35 +1000

    chore: Update .gitignore, req, and  celanup.

--- tests/test.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from ardupilot_log_handler.ap_log_handler import ArduPilotLogHandler
-
-# def test_log_uid_calculation():
-#     handler = ArduPilotLogHandler("tests/test_data/example1.bin")
-#     assert handler.get_log_uid() is not None
